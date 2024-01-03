124ca768c2a2c63582f99cc80faf90a5567ea3e2
Bartlomiej Ozimkiewicz <bartek.ozim@wp.pl>
2024-01-03 10:36:21 +0100

    Lab02

---
+++ lab02/test.py
@@ -0,0 +1,9 @@
+S = {"zegar": {100: 7}, "obraz-pejzaz": {300: 7}, "obraz-portret": {200: 6}, "radio": {40: 2}, "laptop": {500: 5}, "lampka nocna": {70: 6},
+     "srebrne sztućce": {100: 1}, "porcelana": {250: 3}, "figurka z brązu": {300: 10}, "skórzaba torebka": {280: 3}, "odkurzacz": {300: 15}}
+
+
+list_of_products = list(S.values())
+
+
+
+print(len(S))
